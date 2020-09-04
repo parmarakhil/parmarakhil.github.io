645d5e3337dbba57326e1678e5ebb19ae2c4cc2b
akhilparmar2496 <akhilparmar2496@gmail.com>
2020-09-04 20:57:10 +0530

    added firebase

--- docs/environments/environment.prod.ts
+++ docs/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyC2frVRAAltsVTmdwgFnATGHoVUzNHfzsM",
-    authDomain: "live-resume-a575a.firebaseapp.com",
-    databaseURL: "https://live-resume-a575a.firebaseio.com",
-    projectId: "live-resume-a575a",
-    storageBucket: "live-resume-a575a.appspot.com",
-    messagingSenderId: "681076751855",
-    appId: "1:681076751855:web:18bae3866ebfcc4fcd8a1a",
-    measurementId: "G-00VXD77WNG"
+  firebaseConfig = {
+    apiKey: "AIzaSyAb-8MnjHuKcOU7gkghanEMucZzsZJ680U",
+    authDomain: "parmarakhil-15225.firebaseapp.com",
+    databaseURL: "https://parmarakhil-15225.firebaseio.com",
+    projectId: "parmarakhil-15225",
+    storageBucket: "parmarakhil-15225.appspot.com",
+    messagingSenderId: "909406216696",
+    appId: "1:909406216696:web:ee48a17cca2cb2f974360d",
+    measurementId: "G-M2NCFPH9M5"
   }
 };
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require("path").join(__dirname, "./coverage/live-resume"),
+      dir: require("path").join(__dirname, "./coverage/parmarakhil.github.io"),
       reports: ["html", "lcovonly", "text-summary"],
       fixWebpackSourcePaths: true
     },
