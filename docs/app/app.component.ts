d7b5ef69304ffd85999bf1368cf6815ed8d1852d
akhilparmar2496 <akhilparmar2496@gmail.com>
2020-09-04 20:22:21 +0530

    inital setup

---
+++ docs/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from "@angular/core";
+import { Meta, Title } from "@angular/platform-browser";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+
+  title: string = "Akhil Parmar portfolio";
+
+  constructor(
+    private titleService: Title,
+    private metaTagService: Meta
+  ) { }
+
+  ngOnInit(): void {
+
+    this.titleService.setTitle(this.title);
+
+    this.metaTagService.addTags([
+      { name: "keywords", content: "Software developer, Web Developer, Java Developer, Full-stack Java Developer,Automation developer,Camunda Developer, Akhil Parmar portfolio, Akhil Parmar website, Akhil Parmar CV" },
+      { name: "robots", content: "index, follow" },
+      { name: "author", content: "Akhil Parmar" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "date", content: "2020-05-12", scheme: "YYYY-MM-DD" },
+      { charset: "UTF-8" }
+    ]);
+
+    this.metaTagService.updateTag(
+      { name: "description", content: "Hello, I'm a Full-stack Java Web Developer with 2 years of experience developing automation tools using java, angular and camunda" }
+    );
+  }
+}
---
+++ docs/app/welcome/welcome-background/welcome-background.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-welcome-background",
+    templateUrl: "./welcome-background.component.html",
+    styleUrls: [ "./welcome-background.scss", "./welcome-background.reponsivity.scss" ]
+})
+
+export class WelcomeBackgroundComponent implements OnInit {
+    
+    constructor() {}
+
+    ngOnInit() {}
+
+}
