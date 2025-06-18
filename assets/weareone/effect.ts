792c609f728d99b7817c73d1c0abfc0c5fe3c020
parmarakhil <39879212+parmarakhil@users.noreply.github.com>
2025-06-18 17:32:56 +0000

    Deploying to gh-pages from @ parmarakhil/parmarakhil.github.io@4273130246a9be53274d086ad6e44cdae49c5459 🚀

--- assets/weareone/effect.ts
+++
@@ -1,8 +0,0 @@
-export function initEffects() {
-  const balloons = document.getElementById('balloons');
-  if (balloons) {
-    // Animate balloons here
-    console.log('Balloon effect initialized');
-    
-  }
-}
