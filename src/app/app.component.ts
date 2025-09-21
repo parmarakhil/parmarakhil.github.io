ac1cfbce9652793f984d70c4e8cb731378053e97
parmarakhil <akhilparmar2496@gmail.com>
2025-09-21 11:23:38 +0530

    update tags and description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit {
     this.titleService.setTitle(this.title);
 
     this.metaTagService.addTags([
-      { name: "keywords", content: "Software developer, Web Developer, Java Developer, Full-stack Java Developer,Automation developer,Camunda Developer, Akhil Parmar, Akhil Parmar website, Akhil Parmar CV" },
+      { name: "keywords", content: "Mentor, DSA, System design, career transition, Software developer, Web Developer, Java Developer, Full-stack Java Developer,Automation developer,Camunda Developer, Akhil Parmar, Akhil Parmar website, Akhil Parmar CV" },
       { name: "robots", content: "index, follow" },
       { name: "author", content: "Akhil Parmar" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
@@ -29,7 +29,7 @@ export class AppComponent implements OnInit {
     ]);
 
     this.metaTagService.updateTag(
-      { name: "description", content: "Hello, I'm a Full-stack Java Web Developer with 7+ of experience developing automation tools using java, angular and camunda" }
+      { name: "description", content: "Hello, I'm a Full-stack Java Web Developer with 7+ of experience developing highly scaleable distributed softwares and mentoring people to ace in their career." }
     );
   }
 }
